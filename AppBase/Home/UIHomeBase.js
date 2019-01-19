bb76c0c223c20d0d8fb89c0d68dc6bd2ec90fc15
Jaykie <chyfemail163@163.com>
2019-01-19 21:02:08 +0800

    ui

--- AppBase/Home/UIHomeBase.js
+++ AppBase/Home/UIHomeBase.js
@@ -1,5 +1,8 @@
 var UIView = require("UIView");
 var Common = require("Common");
+var TextureCache = require("TextureCache");
+var AppRes = require("AppRes");
+
 cc.Class({
     extends: UIView,
     properties: {
@@ -15,6 +18,11 @@ cc.Class({
             default: null,
             type: cc.Node
         },
+
+        imageBg: cc.Sprite,
+        imageNameBg: cc.Sprite,
+        textName: cc.Label,
+
     },
 
     onLoad: function () {
@@ -39,7 +47,43 @@ cc.Class({
         this.objLayoutBtn.setContentSize(cc.size(w, h));
         this.objLayoutBtn.setPosition(0, y, 0);
 
+        this.textName.string = "ImageNameText";
+
+        var strImage = AppRes.IMAGE_HOME_BG;
+        TextureCache.main.Load(strImage, function (err, tex) {
+            //cc.url.raw('res/textures/content.png')
+            if (err) {
+                cc.log(err.message || err);
+                return;
+            }
+            this.imageBg.spriteFrame = new cc.SpriteFrame(tex);
+            this.LayOut();
+        }.bind(this));
+
+
+        strImage = "Common/UI/UIKit/barbg";
+        TextureCache.main.Load(strImage, function (err, tex) {
+            //cc.url.raw('res/textures/content.png')
+            if (err) {
+                cc.log(err.message || err);
+                return;
+            }
+            this.imageNameBg.spriteFrame = new cc.SpriteFrame(tex);
+            this.LayOut();
+        }.bind(this));
+
+
         cc.log("objLayoutBtn size=" + size);
     },
+
+    LayOut: function () {
+        var size = this.imageBg.node.getContentSize();
+        var x = size.width;
+        var y = size.height;
+        var scale = Common.GetMaxFitScale(x, y, Common.appSceneBase.sizeCanvas.width, Common.appSceneBase.sizeCanvas.height); 
+        this.imageBg.node.scaleX = scale;
+        this.imageBg.node.scaleY = scale;
+    },
+
 });
 
---
+++ Apps/shapecolor/Base/AppRes.js
@@ -0,0 +1,16 @@
+//https://docs.cocos.com/creator/manual/zh/scripting/reference/class.html
+//api: https://docs.cocos.com/creator/api/zh/
+var AppRes = cc.Class({
+    //cc.js.getClassName
+    extends: cc.Object,
+    statics: {
+
+        //image
+        IMAGE_HOME_BG: "App/UI/Home/LearnProgressBg",  
+    },
+
+
+
+});
+
+
