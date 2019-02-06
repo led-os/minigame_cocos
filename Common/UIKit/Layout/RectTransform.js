c8839005f9efc7622cd9ea0198efcf372c58ae79
Jaykie <chyfemail163@163.com>
2019-02-06 15:44:15 +0800

    ui

--- Common/UIKit/Layout/RectTransform.js
+++ Common/UIKit/Layout/RectTransform.js
@@ -24,7 +24,37 @@ var RectTransform = cc.Class({
                 return this._sizeType;
             },
             set: function (value) {
-                return this.UpdateType(value);
+                this.UpdateType(value);
+            },
+        },
+
+        //     The offset of the lower left corner of the rectangle relative to the lower left
+        //     anchor.
+        _offsetMin: cc.Vec2,
+        offsetMin:
+        {
+            type: cc.Vec2,
+            get: function () {
+                return this._offsetMin;
+            },
+            set: function (value) {
+                this._offsetMin = value;
+                this.UpdateType(this.sizeType);
+            },
+        },
+
+        _offsetMax: cc.Vec2,
+        //     The offset of the upper right corner of the rectangle relative to the upper right
+        //     anchor.
+        offsetMax:
+        {
+            type: cc.Vec2,
+            get: function () {
+                return this._offsetMax;
+            },
+            set: function (value) {
+                this._offsetMax = value;
+                this.UpdateType(this.sizeType);
             },
         },
 
@@ -33,16 +63,19 @@ var RectTransform = cc.Class({
     UpdateType: function (type) {
         this._sizeType = type;
         var sizeParent = Common.GetSizeOfParnet(this.node);
+        var x, y, w, h;
 
         switch (this._sizeType) {
             case RectSizeType.MATCH_CONTENT:
                 {
-                    
+
                 }
                 break;
             case RectSizeType.MATCH_PARENT:
                 {
-                    this.node.setContentSize(sizeParent);
+                    w = sizeParent.width - this.offsetMin.x - this.offsetMax.x;
+                    h = sizeParent.height - this.offsetMin.y - this.offsetMax.y;
+                    this.node.setContentSize(new cc.size(w, h));
                 }
                 break;
 
